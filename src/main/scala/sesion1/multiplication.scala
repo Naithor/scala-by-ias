83acfa9634861cc6a67aa98e24417d977244247b
Nicolas Peña <nicolas.arias@ias.com.co>
2020-06-26 17:49:44 -0500

    Power exercise implemented.

--- src/main/scala/sesion1/multiplication.scala
+++ src/main/scala/sesion1/multiplication.scala
@@ -4,22 +4,22 @@ import scala.annotation.tailrec
 import scala.io.StdIn
 
 object multiplication extends App {
-  println("Input a number")
+  println("Ingrersa un numero")
   val number = StdIn.readInt()
 
   multiplication(number)
 
   def multiplication(number: Int): Unit = {
-    val INCREMENTER = 1
+    val INCREMENTER_BY = 1
 
     @tailrec
     def recursionMultiplication(incrementer: Int): Unit = {
       if (incrementer <= 12) {
         println(s"$number x $incrementer = ${number * incrementer}")
-        recursionMultiplication(incrementer + 1)
+        recursionMultiplication(incrementer + INCREMENTER_BY)
       }
     }
 
-    recursionMultiplication(INCREMENTER)
+    recursionMultiplication(INCREMENTER_BY)
   }
 }
